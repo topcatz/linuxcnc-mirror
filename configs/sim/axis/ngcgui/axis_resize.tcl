c4af98d9c4d71e32eba896dca61fb99d9b330eed
Dewey Garrett <dgarrett@panix.com>
2013-12-12 12:56:48 -0700

    ngcgui,pyngcgui: support gcmc files
    Ref:
    gcmc (G-Code Meta Compiler) http://www.vagrearg.org/content/gcmc
    
    With this commit, [py]ngcgui can read .gcmc input files for the
    gcmc compiler and present tagged variables in a [py]ngcgui tab page(s).
    The tabpage info message and compiler options can be specified
    by tags too.
    
    --------------------------------------------------------------------
    Tag formats (simplified):
    
      Variable:
      //ngcgui: vname [= value , [comment text]]
    
      Info message:
      //ngcgui: info: text message
    
      Option:
      //ngcgui: -optioname [optionvalue]
    --------------------------------------------------------------------
    
    When a feature for a gcmc tab page is created, the variables
    and options are passed to the compiler and a gcode subroutine
    file is produced and placed at the head of the linuxcnc subroutine
    search path.  These files are purged at each ngcgui restart
    that uses .gcmc files.
    
    --------------------------------------------------------------------
    gcmc must be installed separately and available in the user PATH.
    --------------------------------------------------------------------
    
    Example ini files for axis, touchy, and gscreen:
       configs/sim/axis/ngcgui/ngcgui_gcmc.ini
       configs/sim/axis/ngcgui/pyngcgui_gcmc.ini
       configs/sim/touchy/ngcgui/pyngcgui_touchy_gcmc.ini
       configs/sim/gscreen/ngcgui/pyngcgui_gscreen_gcmc.ini

---
+++ configs/sim/axis/ngcgui/axis_resize.tcl
@@ -0,0 +1,36 @@
+#-----------------------------------------------------------------------
+# Copyright: 2013
+# Author:    Dewey Garrett <dgarrett@panix.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#-----------------------------------------------------------------------
+
+
+# for axis gui, include ini item:
+# Usage (in ini file):
+# [DISPLAY]USER_COMMAND_FILE = fullscreen.tcl
+
+maxgeo=root_window.tk.call("wm","maxsize",".")
+fullw=maxgeo.split(' ')[0]
+fullh=maxgeo.split(' ')[1]
+fullsize=fullw +'x' + fullh
+
+# custom size (to accomodate bigger gladevcp panels)
+newsize = '800' + 'x' + '680'
+# for fullsize: uncomment this
+# newsize=fullsize
+
+root_window.tk.call("wm","geometry",".",newsize)
+
